ae63c997be5ba157f38e586cf89a848ec255df55
Breval Ferrari <breee@duck.com>
2022-12-08 19:31:21 +0100

    Add an AST and some notes

--- src/file.rs
+++ src/file.rs
@@ -3,11 +3,11 @@ use std::fs;
 pub fn read_file(filename: String) -> String {
 	let data: String = match fs::read_to_string(filename) {
     	Ok(string)=> string,
-    	Err(error)=> panic!("error reading file: {:?}", error)
+    	Err(error)=> panic!("error reading file: {}", error)
     };
     let mut parsed_data: String = match data.parse() {
     	Ok(string)=> string,
-    	Err(error)=> panic!("error parsing file: {:?}", error)
+    	Err(error)=> panic!("error parsing file: {}", error)
     };
     assert_eq!(data, parsed_data);
     parsed_data.pop(); // the last line is always empty
