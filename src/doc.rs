af66fa9667d3fa0592dbc4bc78e04849f1abbec3
Breee <breee@duck.com>
2022-12-11 19:57:40 +0100

    Serialization attempt n°1

--- src/doc.rs
+++ src/doc.rs
@@ -1,3 +1,5 @@
 // Doc lib, library containing all the text of the project.
 
 pub static USAGE: &'static str = "Usage: interbeeper FILENAME";
+
+// TODO: Error descriptions
--- src/proc.rs
+++ src/proc.rs
@@ -1,8 +1,26 @@
-use std::env;
-use crate::doc::USAGE;
+use crate::doc::{USAGE, panic};
+
+enum Word {
+	L(u8),
+	O(u8),
+	N(u8)
+}
+
+struct Channel {
+	title: String,
+	words: Vec<Word>
+}
+
+struct Song {
+	title: String,
+	author: String,
+	scale: Vec<&'static str>,
+	channels: Vec<Channel>,
+	bpm: u16
+}
 
 pub fn get_filename() -> String {
-	let args:Vec<String> = env::args().collect();
+	let args:Vec<String> = std::env::args().collect();
 	if args.len() != 2 {
 		println!("{}", USAGE);
 		panic!("parsing arguments: invalid number of arguments.");
@@ -17,14 +35,58 @@ pub fn get_filename() -> String {
 
 pub fn serialize(data: String) {
 	let mut chars = data.chars();
-	let mut item = chars.next();
-	let mut character: char;
-	while item!=None {
-		character = match item {
-			Some(c) => c,
-			None => '!' // this will never occur
+	let mut characters = vec![&chars.next()];
+
+	let mut song = Song {
+		title: String::new(),
+		author: String::new(),
+		scale: Vec::from(["c","C","d","D","e","f","F","g","G","a","A","b"]),
+		channels: Vec::new(),
+		bpm: 60
+	};
+
+	while !characters[0].is_none() {
+		
+		match
+			match characters[0].unwrap() {
+				't'	=>	{
+					characters.push(&chars.next());
+					match characters[1].unwrap() {
+						'i'	=>	{ // title
+							chars.skip_while(|x| x!=&':');
+							chars.skip_while(|x| x==&' ');
+							characters.push(&chars.next());
+							if characters[2].unwrap()=='\n' {panic(0u8);}
+							while characters[2].unwrap()!='\n' {
+								match chars.next().unwrap() {
+									' '	=>	continue,
+									not_a_space	=>	{
+										song.title.push(match not_a_space {
+											'\\'	=>	{
+												match chars.next().unwrap() {
+													'n'			=>	'\n',
+													other	=>	other
+												}
+											}
+											anything	=>	anything
+										});
+									}
+								};
+							};
+							"Title: {song.title}"
+						},
+						_	=>	""
+					}
+				},
+				_	=>	""
+			}
+
+			{
+				str	=>	println!("{:?}", str),
+				_ => continue
+			};
+		
+			characters = vec![&chars.next()];
+
 		};
-		println!("{:?}", character);
-		item = chars.next();
 	}
-}
