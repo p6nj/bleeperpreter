4b3e27262746aabc4dad7b9f2bf384f95efaf746
p6nj <breee@duck.com>
2023-06-25 22:30:04 +0200

    preparing v. 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use anyhow::{Context, Result};
-
-mod audio;
-mod cereal;
-mod file;
-mod proc;
-mod structs;
-
-fn main() -> Result<()> {
-    let (input, output) = proc::get_filenames().context("Cannot find file names in args")?;
-    let data = file::read(input).context("Cannot process file data")?;
-    let serialized = cereal::serialize(data).context("Cannot parse text")?;
-    println!("{serialized}");
-    // let length = serialized.channels.len() as u16;
-    file::write(output, proc::render(serialized)?, 1)?;
-    Ok(())
+fn main() {
+    println!("Hello World!");
 }
