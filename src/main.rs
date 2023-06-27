d479c75d970ce7e9aafcb3c776c8757bd1929688
p6nj <breee@duck.com>
2023-06-27 16:55:44 +0200

    simple json parsing

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::fs::read_to_string;
 
 mod json;
 
+use ::json::JsonValue;
 use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(version, about = "An advanced MML interpreter")]
 struct Arguments {
-    input_filename: String,
+    json_path: String,
 }
 
 fn main() {
     let args = Arguments::parse();
-    let content = read_to_string(args.input_filename).unwrap();
-    dbg!(content);
+    let content = read_to_string(args.json_path).unwrap();
+    println!("{}", JsonValue::from(content));
 }
