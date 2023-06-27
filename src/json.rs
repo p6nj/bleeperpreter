5f5253d741760ff8fb15ad34fee43360564a4347
p6nj <breee@duck.com>
2023-06-27 16:52:23 +0200

    read file in arg + json validation structure

---
+++ src/json.rs
@@ -0,0 +1,5 @@
+use anyhow::{Context, Error};
+use json::JsonValue;
+pub fn validate(json: JsonValue) -> Result<(), Error> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::read_to_string;
+
+mod json;
+
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[clap(version, about = "An advanced MML interpreter")]
+struct Arguments {
+    input_filename: String,
+}
+
 fn main() {
-    println!("Hello World!");
+    let args = Arguments::parse();
+    let content = read_to_string(args.input_filename).unwrap();
+    dbg!(content);
 }
