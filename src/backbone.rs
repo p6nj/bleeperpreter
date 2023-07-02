cf121e6c6cd49f9a97f0bfd5133994c89d8de2ed
p6nj <breee@duck.com>
2023-07-02 16:32:22 +0200

    mask combinators and tests

--- src/backbone.rs
+++ src/backbone.rs
@@ -1,9 +1,15 @@
-use std::{collections::HashMap, ops::Index};
+use std::collections::HashMap;
 
 use anyhow::{Context, Error};
 use json::JsonValue;
 use meval::{tokenizer::Operation, Expr};
-use nom::{character::complete::one_of, combinator::map_res};
+use nom::branch::alt;
+use nom::character::complete::{char, digit1, space0};
+use nom::multi::many0;
+use nom::{
+    character::complete::one_of, combinator::map_res, number::complete::u8, sequence::preceded,
+    IResult,
+};
 
 pub struct Album {
     name: String,
@@ -50,12 +56,47 @@ struct Channel {
     mask: Mask,
 }
 
+#[derive(PartialEq, Debug)]
 enum MaskAtoms {
     Octave(u8),
     Length(u8),
     Volume(u8),
     Note(u8),
     Rest,
+}
+
+fn note<'a>(notes: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtoms> {
+    map_res(one_of(notes), move |c| {
+        Ok::<MaskAtoms, Error>(MaskAtoms::Note(
+            notes
+                .find(c)
+                .with_context(|| format!("unknown note: {}", c))?
+                .try_into()
+                .context("converting note index to u8 (shouldn't fail)")?,
+        ))
+    })
+}
+
+fn rest<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtoms> {
+    map_res(char('.'), move |_| Ok::<MaskAtoms, Error>(MaskAtoms::Rest))
+}
+
+fn length<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtoms> {
+    map_res(preceded(char('$'), map_res(digit1, str::parse)), move |n| {
+        Ok::<MaskAtoms, Error>(MaskAtoms::Length(n))
+    })
+}
+
+fn octave<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtoms> {
+    map_res(preceded(char('@'), map_res(digit1, str::parse)), move |n| {
+        Ok::<MaskAtoms, Error>(MaskAtoms::Octave(n))
+    })
+}
+
+fn volume<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtoms> {
+    map_res(preceded(char('!'), map_res(digit1, str::parse)), move |n| {
+        Ok::<MaskAtoms, Error>(MaskAtoms::Volume(n))
+    })
 }
 
 struct Mask(Vec<MaskAtoms>);
@@ -70,9 +111,34 @@ impl From<JsonValue> for Channel {
 impl TryFrom<(String, String)> for Mask {
     type Error = Error;
     fn try_from(value: (String, String)) -> Result<Self, Self::Error> {
-        Ok(Self(vec![MaskAtoms::Note(map_res(
-            one_of(value.1),
-            move |note| -> u8 { value.1.find(note).unwrap().try_into() },
-        )(value.0))]))
+        Ok(Mask(
+            many0(preceded(
+                space0,
+                alt((note(value.1.as_str()), rest(), length(), octave(), volume())),
+            ))(value.0.as_str())
+            .context("cannot parse mask input")?
+            .1,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    pub fn note_parser() {
+        assert_eq!(Ok(("iueg", MaskAtoms::Note(2))), note("abcde")("ciueg"));
+    }
+    #[test]
+    pub fn length_parser() {
+        assert_eq!(Ok(("iueg", MaskAtoms::Length(16))), length()("$16iueg"));
+    }
+    #[test]
+    pub fn octave_parser() {
+        assert_eq!(Ok(("iueg", MaskAtoms::Octave(4))), octave()("@4iueg"));
+    }
+    #[test]
+    pub fn rest_parser() {
+        assert_eq!(Ok(("iueg", MaskAtoms::Rest)), rest()(".iueg"));
     }
 }
