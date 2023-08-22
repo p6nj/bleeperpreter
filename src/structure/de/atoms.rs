c69d1e06c1d239eada5203be31cef4d09d6fa19d
p6nj <breee@duck.com>
2023-08-22 18:12:59 +0200

    constants & adaptative tests

--- src/structure/de/atoms.rs
+++ src/structure/de/atoms.rs
@@ -11,6 +11,17 @@ use std::num::NonZeroU8;
 #[cfg(test)]
 mod tests;
 
+const OCTAVE: char = '@';
+const LENGTH: char = '$';
+const VOLUME: char = '!';
+const REST: char = '.';
+const OCTAVEINCR: char = '>';
+const OCTAVEDECR: char = '<';
+const LENGTHINCR: char = '/';
+const LENGTHDECR: char = '\\';
+const VOLUMEINCR: char = '^';
+const VOLUMEDECR: char = '_';
+
 #[derive(PartialEq, Debug, Clone)]
 pub(crate) enum MaskAtom {
     Octave(NonZeroU8),
@@ -32,7 +43,7 @@ type LeResult<'a> = IResult<&'a str, MaskAtom>;
 fn octave(i: &str) -> IResult<&str, MaskAtom> {
     map_res(
         map_opt(
-            verify(preceded(char('@'), u8), |n| NonZeroU8::new(*n).is_some()),
+            verify(preceded(char(OCTAVE), u8), |n| NonZeroU8::new(*n).is_some()),
             NonZeroU8::new,
         ),
         |n| R::Ok(MaskAtom::Octave(n)),
@@ -40,11 +51,15 @@ fn octave(i: &str) -> IResult<&str, MaskAtom> {
 }
 
 fn length(i: &str) -> IResult<&str, MaskAtom> {
-    map_res(preceded(char('$'), u8), move |n| R::Ok(MaskAtom::Length(n)))(i)
+    map_res(preceded(char(LENGTH), u8), move |n| {
+        R::Ok(MaskAtom::Length(n))
+    })(i)
 }
 
 fn volume(i: &str) -> IResult<&str, MaskAtom> {
-    map_res(preceded(char('!'), u8), move |n| R::Ok(MaskAtom::Volume(n)))(i)
+    map_res(preceded(char(VOLUME), u8), move |n| {
+        R::Ok(MaskAtom::Volume(n))
+    })(i)
 }
 
 fn note<'a>(notes: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtom> {
@@ -54,31 +69,31 @@ fn note<'a>(notes: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, MaskAtom>
 }
 
 fn rest(i: &str) -> LeResult {
-    value(MaskAtom::Rest, char('.'))(i)
+    value(MaskAtom::Rest, char(REST))(i)
 }
 
 fn octaveincr(i: &str) -> LeResult {
-    value(MaskAtom::OctaveIncr, char('>'))(i)
+    value(MaskAtom::OctaveIncr, char(OCTAVEINCR))(i)
 }
 
 fn octavedecr(i: &str) -> LeResult {
-    value(MaskAtom::OctaveDecr, char('<'))(i)
+    value(MaskAtom::OctaveDecr, char(OCTAVEDECR))(i)
 }
 
 fn lengthincr(i: &str) -> LeResult {
-    value(MaskAtom::LengthIncr, char('\\'))(i)
+    value(MaskAtom::LengthIncr, char(LENGTHINCR))(i)
 }
 
 fn lengthdecr(i: &str) -> LeResult {
-    value(MaskAtom::LengthDecr, char('/'))(i)
+    value(MaskAtom::LengthDecr, char(LENGTHDECR))(i)
 }
 
 fn volumeincr(i: &str) -> LeResult {
-    value(MaskAtom::VolumeIncr, char('^'))(i)
+    value(MaskAtom::VolumeIncr, char(VOLUMEINCR))(i)
 }
 
 fn volumedecr(i: &str) -> LeResult {
-    value(MaskAtom::VolumeDecr, char('_'))(i)
+    value(MaskAtom::VolumeDecr, char(VOLUMEDECR))(i)
 }
 
 fn junk(i: &str) -> IResult<&str, ()> {
--- src/structure/de/atoms/tests.rs
+++ src/structure/de/atoms/tests.rs
@@ -3,49 +3,76 @@ use super::*;
 fn octave() {
     assert_eq!(
         Ok(("", MaskAtom::Octave(NonZeroU8::new(2).unwrap()))),
-        super::octave("@2")
+        super::octave(format!("{OCTAVE}2").as_str())
     );
 }
-// #[test]
-// fn length() {
-//     assert_parses("$4", MaskAtom::Length(4), None);
-// }
-// #[test]
-// fn volume() {
-//     assert_parses("!16", MaskAtom::Volume(16), None);
-// }
-// #[test]
-// fn note() {
-//     assert_parses("e", MaskAtom::Note(4), Some("abcde"));
-// }
-// #[test]
-// fn rest() {
-//     assert_parses(".", MaskAtom::Rest, None);
-// }
-// #[test]
-// fn octave_incr() {
-//     assert_parses(">", MaskAtom::OctaveIncr, None);
-// }
-// #[test]
-// fn octave_decr() {
-//     assert_parses("<", MaskAtom::OctaveDecr, None);
-// }
-// #[test]
-// fn length_incr() {
-//     assert_parses("\\", MaskAtom::LengthIncr, None);
-// }
-// #[test]
-// fn length_decr() {
-//     assert_parses("/", MaskAtom::LengthDecr, None);
-// }
-// #[test]
-// fn volume_incr() {
-//     assert_parses("^", MaskAtom::VolumeIncr, None);
-// }
-// #[test]
-// fn volume_decr() {
-//     assert_parses("_", MaskAtom::VolumeDecr, None);
-// }
+#[test]
+fn length() {
+    assert_eq!(
+        Ok(("", MaskAtom::Length(4))),
+        super::length(format!("{LENGTH}4").as_str())
+    );
+}
+#[test]
+fn volume() {
+    assert_eq!(
+        Ok(("", MaskAtom::Volume(100))),
+        super::volume(format!("{VOLUME}100").as_str())
+    );
+}
+#[test]
+fn note() {
+    assert_eq!(Ok(("", MaskAtom::Note(2))), super::note("abcde")("c"));
+}
+#[test]
+fn rest() {
+    assert_eq!(
+        Ok(("", MaskAtom::Rest)),
+        super::rest(format!("{REST}").as_str())
+    );
+}
+#[test]
+fn octave_incr() {
+    assert_eq!(
+        Ok(("", MaskAtom::OctaveIncr)),
+        super::octaveincr(format!("{OCTAVEINCR}").as_str())
+    );
+}
+#[test]
+fn octave_decr() {
+    assert_eq!(
+        Ok(("", MaskAtom::OctaveDecr)),
+        super::octavedecr(format!("{OCTAVEDECR}").as_str())
+    );
+}
+#[test]
+fn length_incr() {
+    assert_eq!(
+        Ok(("", MaskAtom::LengthIncr)),
+        super::lengthincr(format!("{LENGTHINCR}").as_str())
+    );
+}
+#[test]
+fn length_decr() {
+    assert_eq!(
+        Ok(("", MaskAtom::LengthDecr)),
+        super::lengthdecr(format!("{LENGTHDECR}").as_str())
+    );
+}
+#[test]
+fn volume_incr() {
+    assert_eq!(
+        Ok(("", MaskAtom::VolumeIncr)),
+        super::volumeincr(format!("{VOLUMEINCR}").as_str())
+    );
+}
+#[test]
+fn volume_decr() {
+    assert_eq!(
+        Ok(("", MaskAtom::VolumeDecr)),
+        super::volumedecr(format!("{VOLUMEDECR}").as_str())
+    );
+}
 // #[test]
 // fn loop_() {
 //     assert_parses("(ccc)", MaskAtom::Loop(vec![]), None);
