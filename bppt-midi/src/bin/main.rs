0187bc71a2ba7883e492a4a344bd4c2a657182fc
p6nj <breee@duck.com>
2023-09-10 23:28:10 +0200

    test having multiple tracks

--- bppt-midi/src/bin/main.rs
+++ bppt-midi/src/bin/main.rs
@@ -1,6 +1,5 @@
 use apres::MIDI;
 use basic_toml;
-use bppt_midi::misc::bpm_to_tempo;
 use bppt_midi::playback::play;
 use bppt_midi::structs::Song;
 use rustysynth::SoundFont;
@@ -25,7 +24,8 @@ fn main() {
     //     from_str::<Song>(&read_to_string("bppt-midi/toml/poc.toml").unwrap()).unwrap()
     // );
 
-    let midi_file = MIDI::from_path("bppt-midi/midi/C at 60 BPM oboe.mid").unwrap();
+    let midi_file = MIDI::from_path("bppt-midi/midi/2 parts.mid").unwrap();
+    dbg!(&midi_file);
     let song: Song = basic_toml::from_str(
         read_to_string(Path::new("bppt-midi/toml/poc.toml"))
             .unwrap()
